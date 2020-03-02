1717c9198412c76a638924f00f2929846a5992d8
wwlee94 <wwlee94@naver.com>
2020-03-02 23:37:40 +0900

    PUT /todos/:todoId - 할일 업데이트 기능 추가

--- src/router/todos.js
+++ src/router/todos.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.get('/', findAllTodo);
 router.get('/:todoId', findTodoById);
 router.post('/', validateParams, createTodo);
+router.put('/:todoId', updateTodo);
+router.put('/:todoId/complete', updateTodoComplete);
 
 // 모든 할일 검색
 function findAllTodo(req, res, next){
@@ -31,9 +33,9 @@ function findTodoById(req, res, next){
 
 // 파라미터 검증
 function validateParams(req, res, next){
-    if (!Object.keys(req.body).length) return next(new exception.NotFoundParameterError('파라미터가 비어있습니다 !'));
-    if (!req.body.title) return next(new exception.NotFoundParameterError('할일 제목을 입력해주세요 !'));
-    if (!req.body.description) return next(new exception.NotFoundParameterError('할일 내용을 입력해주세요 !'));
+    if (!Object.keys(req.body).length) return next(new exception.NotFoundParameterError('등록에 필요한 파라미터 정보가 없습니다. !'));
+    if (!req.body.title) return next(new exception.InvalidParameterError('할일 제목을 입력해주세요 !'));
+    if (!req.body.description) return next(new exception.InvalidParameterError('할일 내용을 입력해주세요 !'));
     else next();
 };
 
@@ -41,10 +43,35 @@ function validateParams(req, res, next){
 function createTodo(req, res, next){
     const todo = new Todo(req.body);
     todo.save()
-        .then(todo => {
-            res.send(todo);
-        })
+        .then(todo => { res.send(todo); })
         .catch(err => { return next(new exception.ExceptionError(err.message)); });
 };
 
+// 할일 수정
+function updateTodo(req, res, next){
+    // 넘어온 ID로 저장된 할일이 있는지 검사후 저장
+    Todo.findOne({ "id": req.params.todoId })
+        .then(todo => { validateTodoAndUpdate(todo, req, res); })
+        .catch(err => { 
+            if (err instanceof exception.ExceptionError) return next(err);
+            return next(new exception.ExceptionError(err.message));
+         });
+};
+
+// 검증 후 기존 데이터를 수정
+function validateTodoAndUpdate(todo, req, res){
+    let keys = Object.keys(req.body);
+    if (!todo) throw  new exception.NotFoundDataError('존재하지 않는 ID입니다 !');
+    if (!keys.length) throw new exception.NotFoundParameterError('변경에 필요한 파라미터 정보가 없습니다 !');
+
+    keys.forEach(key => { todo[key] = req.body[key]; });
+    todo.save()
+        .then(todo => { res.send(todo); })
+        .catch(err => { throw new exception.ExceptionError(err.message); });
+}
+
+// 할일 완료
+function updateTodoComplete(req, res, next){
+    
+}
 export default router;
