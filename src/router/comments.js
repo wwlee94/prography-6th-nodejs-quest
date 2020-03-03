f08594b2bef1cc4a1c67aace92fc4e792240aea1
wwlee94 <wwlee94@naver.com>
2020-03-03 23:10:38 +0900

    DELETE /todos/:todoId/comments/:commentId - 댓글 삭제 API 추가

--- src/router/comments.js
+++ src/router/comments.js
@@ -9,6 +9,7 @@ router.post('/:todoId/comments', validateParams, createComment);
 router.get('/:todoId/comments', findAllByCommentById);
 router.get('/:todoId/comments/:commentId', findCommentById);
 router.put('/:todoId/comments/:commentId', updateComment);
+router.delete('/:todoId/comments/:commentId', deleteCommentById);
 
 // 파라미터 검증
 async function validateParams(req, res, next) {
@@ -74,16 +75,30 @@ async function updateComment(req, res, next) {
     if (!req.body.contents) return next(new exception.InvalidParameterError('수정할 댓글 내용을 입력해주세요 !'));
     try {
         let comment = await Comment.findOneAndUpdate(
-                { id: Number(req.params.commentId), todoId: Number(req.params.todoId) },
-                { contents: req.body.contents },
-                { new: true }
-            );
+            { id: Number(req.params.commentId), todoId: Number(req.params.todoId) },
+            { contents: req.body.contents },
+            { new: true }
+        );
+        if (!comment) return next(new exception.NotFoundDataError('해당 ID로 검색된 댓글이 없습니다. 다시 입력해주세요 !'));
         res.send(comment);
         // Chaining 방식
         // let comment = await Comment
         //     .where('id').equals(req.params.commentId)
         //     .where('todoId').equals(req.params.todoId)
         //     .update({ contents: req.body.contents }).setOptions({ new: true});
+    } catch (err) {
+        return next(new exception.ExceptionError(err.message));
+    }
+};
+
+// 댓글 삭제
+async function deleteCommentById(req, res, next){
+    try {
+        let comment = await Comment.findOneAndDelete()
+            .where('id').equals(req.params.commentId)
+            .where('todoId').equals(req.params.todoId);
+        if (!comment) return next(new exception.NotFoundDataError('해당 ID로 검색된 댓글이 없습니다. 다시 입력해주세요 !'));
+        res.send({ "msg": "success" });
     } catch (err) {
         return next(new exception.ExceptionError(err.message));
     }
